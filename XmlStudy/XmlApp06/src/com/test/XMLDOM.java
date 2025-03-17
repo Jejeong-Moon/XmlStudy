780b1cabfbecb73c61bf1922cd607b62852473cb
Jejeong-Moon <monjing@naver.com>
2025-03-17 10:41:57 +0900

    Xmlstudy

---
+++ XmlStudy/XmlApp06/src/com/test/XMLDOM.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class XMLDOM
+{
+	public static String getText(Element parent, String tagName)
+	{
+		String result = "";
+		
+		Node node = parent.getElementsByTagName(tagName).item(0);
+		Element element = (Element)node;
+		
+		result = element.getChildNodes().item(0).getNodeValue();
+		
+		return result;
+	}
+	
+	public static String getContext(Element parent, String tagName)
+	{
+		String result = "";
+		
+		Node node = parent.getElementsByTagName(tagName).item(0);
+		Element element = (Element)node;
+		
+		
+		//System.out.printf("%s%n%n",element.getTextContent().replaceAll("<br />", "\n"));
+		
+		result = element.getTextContent().replaceAll("<br />", "\n");
+		return result;
+	}
+	
+	
+}
